d8e794df4c253f8d10d6706717e20f4ee58b83d2
justin20123 <114899999+justin20123@users.noreply.github.com>
2023-07-02 17:14:19 +0700

    cart coba checkout (error di alert)

--- resources/views/product/cart.blade.php
+++ resources/views/product/cart.blade.php
@@ -8,29 +8,39 @@
 </div>
 
     <div class="card card-registration card-registration-2" style="border-radius: 15px;">
-        @foreach (session('cart') as $id =>$details)
-            <div class="row mb-4 d-flex justify-content-between align-items-center">
-                <div class="col-md-2 col-xl-2">
-                    <img src="{{$details['image']}}" class="img-fluid rounded-3" alt="{{$details['name']}}">
+        @if (session('cart'))
+            @foreach (session('cart') as $id =>$details)
+                <div class="row mb-4 d-flex justify-content-between align-items-center">
+                    <div class="col-md-2 col-xl-2">
+                        <img src="{{$details['image']}}" class="img-fluid rounded-3" alt="{{$details['name']}}">
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xl-3">
+                        <h6 class="text-black mb-0">{{$details['name']}}</h6>
+                        <h6 class="text-muted">{{App\Http\Controllers\ProductController::rupiah($details['price'])}}</h6>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xl-2">
+                        <input id="quantity{{ $details['idProduk'] }}" min="0" name="quantity" value="{{$details['quantity']}}" type="number" class="form-control form-control-sm" onchange="updateQuantity({{ $details['idProduk'] }})"/>
+                        <h6 id="stock{{$details['idProduk']}}" style="color: {{ $details['stock'] < 5 ? 'red' : 'inherit' }}" value="{{ $details['stock']}}">Stock : {{ $details['stock'] }}</h6>
+                    </div>
+                    <div class="col-md-3 col-lg-2 col-xl-2">
+                        <h6 class="mb-0">{{App\Http\Controllers\ProductController::rupiah($details['price'] * $details['quantity'])}}</h6>
+                    </div>
+                    <div class="col-xl-1 text-end">
+                        <button onclick="removeProductCart({{ $details['idProduk'] }})" style="border: none;"><i class='bx bx-trash'></i></button>
+                    </div>
                 </div>
-                <div class="col-md-3 col-lg-3 col-xl-3">
-                    <h6 class="text-black mb-0">{{$details['name']}}</h6>
-                    <h6 class="text-muted">{{App\Http\Controllers\ProductController::rupiah($details['price'])}}</h6>
-                </div>
-                <div class="col-md-3 col-lg-3 col-xl-2">
-                    <input id="quantity{{ $details['idProduk'] }}" min="0" name="quantity" value="{{$details['quantity']}}" type="number" class="form-control form-control-sm" onchange="updateQuantity({{ $details['idProduk'] }})"/>
-                    <h6 style="color: {{ $details['stock'] < 5 ? 'red' : 'inherit' }}">Stock : {{ $details['stock'] }}</h6>
-                </div>
-                <div class="col-md-3 col-lg-2 col-xl-2">
-                    <h6 class="mb-0">{{App\Http\Controllers\ProductController::rupiah($details['price'] * $details['quantity'])}}</h6>
-                </div>
-                <div class="col-xl-1 text-end">
-                    <button onclick="removeProductCart({{ $details['idProduk'] }})" style="border: none;"><i class='bx bx-trash'></i></button>
-                </div>
+            @endforeach
+            <button type="button" class="btn btn-primary" onclick="doCheckout()">Checkout</button>
+        @else
+            <div class="alert alert-primary" role="alert">
+                <strong>No products in cart</strong>
             </div>
-        @endforeach
+
+
+        @endif
+
     </div>
-    <button type="button" class="btn btn-primary" onclick="doChechout()">Checkout</button>
+
 @endsection
 
 @section('script')
@@ -66,10 +76,25 @@
     }
 
     function doCheckout(){
-        for(val i=0;i<session('cart');i++){
-            if($("#quantity{{ $details['idProduk'] }}").val()>;)
+        let session = JSON.parse('<?php echo json_encode(session('cart')) ?>');
+        if (session){
+            var status = "success";
+
+            for(let i=0;i<session.length;i++){
+                alert($("#quantity" + session('cart')[i]["idProduk"]).val() + $("#stock" + session('cart')[i]["idProduk"]).val());
+                if($("#quantity" + session('cart')[i]["idProduk"]).val()>$("#stock" + session('cart')[i]["idProduk"]).val()){
+                    alert('Jumlah melebihi stok');
+                    status = "gagal";
+                    break;
+                }
+
+            }
+            if(status == "success"){
+                alert("Thank you for your purchase!");
+            }
         }
-        alert("Thank you for your purchase!");
+
+
     }
 </script>
 @endsection
